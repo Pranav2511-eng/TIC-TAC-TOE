6af8b8515d3e11d7f3a7813d8776e5511b4184d6
Pranav2511-eng <pv5291@srmist.edu.in>
2026-04-21 15:25:24 +0530

    uc3 implementaton done

--- src/main/java/tictactoe.java
+++ src/main/java/tictactoe.java
@@ -1,12 +1,39 @@
 import java.util.Random;
+import java.util.Scanner;
 public class tictactoe {
+     static char[][] board = new char[3][3];
     static boolean isHumanTurn;
     static char humanSymbol;
     static char computerSymbol;
 
     public static void main(String[] args) {
+        initializeBoard();
         tossAndAssignSymbols();
+        printBoard();
         displayTossResult();
+
+        int slot = getUserSlot();
+        System.out.println("Slot entered: " + slot);
+    }
+
+    static void initializeBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = '-';
+            }
+        }
+    }
+
+    static void printBoard() {
+        System.out.println("-------------");
+        for (int row = 0; row < 3; row++) {
+            System.out.print("| ");
+            for (int col = 0; col < 3; col++) {
+                System.out.print(board[row][col] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
     }
 
     static void tossAndAssignSymbols() {
@@ -31,5 +58,11 @@ public class tictactoe {
 
         System.out.println("Human symbol: " + humanSymbol);
         System.out.println("Computer symbol: " + computerSymbol);
+    }
+
+    static int getUserSlot() {
+        Scanner sc = new Scanner(System.in);
+        int slot = sc.nextInt();
+        return slot;
     }
 }
